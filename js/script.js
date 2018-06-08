835a3e2ac6a49431173ee09852b1a792f1d4be35
Jesse Lamas Calvillo <jesse.calvillo@gmail.com>
2018-06-08 01:25:57 -0700

    data loads into js properly

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
-// Anything we want here
+
+//************************* TREE ***************************//
+d3.json("data/exports2014.json", function(error, data){
+  if (error) throw error;
+
+  // console.log(data);
+
+  // data.forEach(function(d){
+  //             console.log("2014 Wine Exports",
+  //                             d.country,
+  //                             d.export_value_USD,
+  //                             "USD");
+  //             });
+
+  // our TREE-MAP here
+
+});
+
+//************************* SCATTER ***************************//
+
+d3.json("data/winemag-data-130k-v2.json", function(error, data){
+  if (error) throw error;
+
+  // console.log(data);
+
+  // data.forEach(function(d){
+  //             console.log(d.country,
+  //                         d.points,
+  //                         d.variety);
+  //             });
+
+  // our WEIGHTED SCATTER PLOT HERE
+
+});
